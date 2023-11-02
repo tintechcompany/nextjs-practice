c2c2f4fed27148968ea3d70617809cbedb2d39bb
ali <ali@inpress.be>
2023-11-02 09:59:05 +0100

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,31 @@
-import { ClerkProvider } from '@clerk/nextjs'
+import {ClerkProvider} from '@clerk/nextjs'
 import React from "react";
+import {Metadata} from "next";
+import './global.css'
+import {Inter, Space_Grotesk} from "next/font/google";
+
+
+ const inter = Inter({
+    subsets: ['latin'],
+    weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+    variable: '--font-inter'
+
+})
+
+ const spaceGrotesk = Space_Grotesk({
+    subsets: ['latin'],
+    weight: ['300', '400', '500', '600', '700'],
+    variable: '--font-spaceGrotesk'
+
+})
+export const metadata: Metadata = {
+    title: 'AliNextJS',
+    description: 'Ali NextJS description',
+
+    icons: {
+        icon: '/assets/images/site-logo.png'
+    }
+}
 
 export default function RootLayout({
                                        children,
@@ -7,11 +33,23 @@ export default function RootLayout({
     children: React.ReactNode
 }) {
     return (
-        <ClerkProvider>
+        <ClerkProvider appearance={{
+            elements : {
+                formButtonPrimary:'primary-gradient',
+                footerActionLink:'primary-text-gradient hover:text-primary-500',
+
+            }
+        }}>
             <html lang="en">
-            <body>{children}</body>
+            <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+            <h1 className={'h1-bold'}>This is text</h1>
+
+
+            {children}
+            </body>
 
             </html>
         </ClerkProvider>
-    )
+    );
+
 }
